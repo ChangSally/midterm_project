a6b04ac48e65d6542cda9f9ec395adbb8d2df3c0
gingerlin <gingerlinplus@gmail.com>
2017-04-25 16:37:30 +0800

    each character(4)

---
+++ html/scripts/main.js
@@ -0,0 +1,10 @@
+<script language="javascript">
+document.write('<img src="img/ca.png">');
+</script>
+
+<script language="javascript">
+function ShowMeDate() {
+  document.write('<img src="img/Characters/insideout/char_3.png">');
+}
+</script>
+<button onclick="ShowMeDate()">告訴我今天日期</button>
